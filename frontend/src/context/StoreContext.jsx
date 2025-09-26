06d1ed1093736d198f5ef6d41e5407ef9e9161af
Raksha SP <spraksha31@gmail.com>
2025-09-26 10:08:23 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
 
-  const url = "https://bhojana-backend.onrender.com/";
+  const url = "https://bhojana-backend.onrender.com";
 
   const [token, setToken] = useState("");
 
